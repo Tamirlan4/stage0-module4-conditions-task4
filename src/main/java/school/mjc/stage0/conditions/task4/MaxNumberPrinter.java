99d1b32a2b02ca81bf96b6a48450211e84a5e6dd
Tamirlan4 <tamirlan_agubaev@mail.ru>
2022-10-20 10:53:14 +0600

    20 10 20222 10:52

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int max = 0;
+        max = first > second && first > third ?
+                first:
+                max;
+        max = second > first && second > third ?
+                second:
+                max;
+        max = third > first && third > second ?
+                third:
+                max;
+        max = max == 0?first:max;
+
+        System.out.println(max);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int max = 0;
+        max = first > second && first > third ?
+                first:
+                max;
+        max = second > first && second > third ?
+                second:
+                max;
+        max = third > first && third > second ?
+                third:
+                max;
+        max = max == 0?first:max;
+        System.out.println(max);
     }
 }
